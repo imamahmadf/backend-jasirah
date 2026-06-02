633f605d7be0f5a11c7e737a6b90d246cb96821f
imamahmadf <fahruraziimam22@gmail.comm>
2026-06-02 16:17:30 +0800

    fix Presensis

--- src/migrations/20260517085117-create-presensi.js
+++ src/migrations/20260517085117-create-presensi.js
@@ -1,46 +1,46 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Presensis', {
+    await queryInterface.createTable("presensis", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       pegawaiId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       jamMasuk: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       jamPulang: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       latitudeMasuk: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       longitudeMasuk: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       latitudePulang: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       longitudePulang: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Presensis');
-  }
+    await queryInterface.dropTable("presensis");
+  },
 };
