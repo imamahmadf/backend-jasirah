639727e58a5f77e96e75cb5bca01fc56ca36a177
imamahmadf <fahruraziimam22@gmail.comm>
2026-07-07 12:57:15 +0800

    fix migration

--- src/migrations/20260701025412-add-statusUnitkerja.js
+++ src/migrations/20260701025412-add-statusUnitkerja.js
@@ -2,13 +2,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("daftarUnitkerjas", "status", {
+    await queryInterface.addColumn("daftarUnitKerjas", "status", {
       type: Sequelize.ENUM("gudang", "non-gudang"),
       allowNull: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("daftarUnitkerjas", "status");
+    await queryInterface.removeColumn("daftarUnitKerjas", "status");
   },
 };
